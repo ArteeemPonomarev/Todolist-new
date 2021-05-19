714fd82c1d6a52d8ca5ae094240cab62486777c0
ArteeemPonomarev <artponomarev1994@gmail.com>
2021-05-19 23:44:14 +0300

    story AppWithRedux with decorator

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
-import {action} from '@storybook/addon-actions';
 import AppWithRedux from '../AppWithRedux';
 import {ReduxStoreProviderDecorator} from './ReduxStoreProviderDecorator';
+
 
 export default {
   title: 'Todo/AppWithRedux',
--- src/stories/ReduxStoreProviderDecorator.tsx
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -3,6 +3,6 @@ import {Provider} from 'react-redux';
 import {store} from '../state/store';
 
 
-export const ReduxStoreProviderDecorator = (storyFn: any) => {
-  return <Provider store={store}>{storyFn}</Provider>
+export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
+  return <Provider store={store}>{storyFn()}</Provider>
 }
